9b437d192c947b76b9d02dd67d78367835c895f3
Claudio Vallejo <claudio.vallejo@gmail.com>
2018-12-10 10:11:58 -0300

    agrego meta para verificacion de google

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta name="google-site-verification" content="F2mXWWKU-a2-hpybnJUsIaaXcVfAOpGpfNCR8Yd-GM8" />
+        
         <title>Garagewine</title>
 
         <!-- Fonts -->
